663cde2e98d575c56d2a6e7330a6e85a44621bd3
FauziSeptians <fauzibelajar96@gmail.com>
2025-11-23 23:18:37 +0700

    chore: setup library zsanss-ui

--- lib/main.ts
+++
@@ -1,8 +0,0 @@
-import { Builder } from "../lib/utils/design-patern/builder";
-import factory from "./utils/design-patern/factory";
-
-export default {
-      Builder,
-      factory
-}
-
---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from "node:path";
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import dts from "vite-plugin-dts";
+import tsconfigPaths from "vite-tsconfig-paths";
+var __dirname = dirname(fileURLToPath(import.meta.url));
+export default defineConfig({
+    plugins: [react(), tsconfigPaths(), dts({
+            tsconfigPath: "./tsconfig.app.json",
+            include: ["lib"],
+            insertTypesEntry: true
+        })],
+    resolve: {
+        alias: [{
+                find: "@",
+                replacement: path.resolve(__dirname, "lib")
+            }]
+    },
+    build: {
+        copyPublicDir: false,
+        lib: {
+            name: "zsanss-core",
+            fileName: "zsanss-core",
+            entry: path.resolve(__dirname, "./src/lib/main.ts"),
+            formats: ["es", "umd"]
+        }
+    },
+});
