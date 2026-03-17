f6de2727b20a2c1d282fb3caa31bee1a13cf37d5
muhammad.putra.ext <muhammad.putra.ext@bcadigital.co.id>
2026-03-17 23:06:17 +0700

    chore: main export function

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -1,37 +1,37 @@
-import {Builder} from "../lib/utils/design-patern/builder";
-import factory from "./utils/design-patern/factory";
-import formatCurrency from "./utils/number/format-currency";
-import { mean } from "./utils/number/mean";
-import { median } from "./utils/number/median";
-import percentage from "./utils/number/percentage";
-import roundTo from "./utils/number/round-to";
-import { sum } from "./utils/number/sum";
-import { uniqueArray } from "./utils/number/unique-array";
-import { ArrayHandler } from "./utils/object/array-handler";
-import flattenArray from "./utils/object/flatten-array";
-import objectClone from "./utils/object/object-clone";
-import { ObjectHandler } from "./utils/object/object-handler";
-import capitalize from "./utils/string/capitalize";
-import maskEmail from "./utils/string/maskEmail";
-import slugify from "./utils/string/slugify";
-import truncate from "./utils/string/truncate";
+// --- Design Patterns ---
+export { Builder } from "../lib/utils/design-patern/builder";
+export { default as factory } from "./utils/design-patern/factory";
 
-export default {
-  Builder,
-  factory,
-  uniqueArray,
-  formatCurrency,
-  mean,
-  median,
-  percentage,
-  roundTo,
-  sum,
-  capitalize,
-  maskEmail,
-  slugify,
-  truncate,
-  ArrayHandler,
-  flattenArray,
-  objectClone,
-  ObjectHandler
-};
+// --- Number Utils ---
+export { default as formatCurrency } from "./utils/number/format-currency";
+export { mean } from "./utils/number/mean";
+export { median } from "./utils/number/median";
+export { default as percentage } from "./utils/number/percentage";
+export { default as roundTo } from "./utils/number/round-to";
+export { sum } from "./utils/number/sum";
+export { uniqueArray } from "./utils/number/unique-array";
+
+// --- Object & Array Utils ---
+export { ArrayHandler } from "./utils/object/array-handler";
+export { default as flattenArray } from "./utils/object/flatten-array";
+export { default as objectClone } from "./utils/object/object-clone";
+export { ObjectHandler } from "./utils/object/object-handler";
+
+// --- String Utils ---
+export { default as capitalize } from "./utils/string/capitalize";
+export { default as maskEmail } from "./utils/string/maskEmail";
+export { default as slugify } from "./utils/string/slugify";
+export { default as truncate } from "./utils/string/truncate";
+
+// --- Hooks & Services ---
+export { BaseApiService } from "./utils/services/baseApiService";
+export { useAutoCarousel } from "./utils/hooks/useAutoCarousel";
+export { useBaseInfiniteQuery } from "./utils/hooks/useBaseInfiniteQuery";
+export { useDataTableMultiQuery } from "./utils/hooks/useDataTableMultipleQueries";
+export { useModal } from "./utils/hooks/useModal";
+export { useOnlineStatus } from "./utils/hooks/useOnlineStatus";
+export { useOptimisticAction } from "./utils/hooks/useOptimisticAction";
+export { usePagination } from "./utils/hooks/usePagination";
+export { useScroll } from "./utils/hooks/useScroll";
+export { createStepper } from "./utils/hooks/useStepper";
+export { useSyncForm } from "./utils/hooks/useSyncForm";
